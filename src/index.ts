2622a1966d8aaf860fe651b93ca56fe3f50cfb21
hewenguang <wenguang.fe@gmail.com>
2024-08-24 16:48:43 +0800

    chore(v1.2.0): release v1.2.0

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,6 @@ export { default as Align } from './float/align';
 export { default as Popup } from './popup';
 export { default as Panel } from './panel';
 export { default as Sidebar } from './sidebar';
+export { default as Outline } from './outline';
 export { default as PanelByFloatbox } from './panel_by_floatbox';
 export { default as SearchByFloatbox } from './search_by_floatbox';
